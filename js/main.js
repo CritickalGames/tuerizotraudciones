fcc1db83f8cfc85611acda28a2d810337b435c27
Critickal Games <zgtale1@hotmail.com>
2024-04-03 21:16:46 -0300

    comeback

--- js/main.js
+++ js/main.js
@@ -1,8 +1,1 @@
-document.querySelector('#pdffFile').addEventListener('change', () => {
-
-    let pdffFile = document.querySelector('#pdffFile').files[0];
-    let pdffFileURL = URL.createObjectURL(pdffFile) + "#toolbar=0";
-
-    document.querySelector('#vistaPrevia').setAttribute('src', "https://github.com/CritickalGames/tuerizotraudciones/blob/main/04.pdf");
-})
 document.querySelector('#vistaPrevia').setAttribute('src', "04.pdf");
